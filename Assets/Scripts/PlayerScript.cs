aa6724e70e607d2931d82fc9740c9ccd3355cd1a
Thomas Kirz <thomas.kirz@gmail.com>
2024-12-19 14:02:28 +0100

    player stuff

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -9,29 +9,30 @@ public class PlayerScript : MonoBehaviour
     
     public float moveSpeed = 6;
     
+    [Header("Sprites")]
+    [SerializeField] private Sprite upSprite;
+    [SerializeField] private Sprite downSprite;
+    [SerializeField] private Sprite sideSprite;
+    
     private Rigidbody2D _rigidbody;
-    private Camera _viewCamera;
     private Vector2 _velocity;
+    private SpriteRenderer _spriteRenderer;
 
     void Start()
     {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
         _rigidbody = GetComponent<Rigidbody2D>();
         if (_rigidbody == null)
         {
             Debug.Log("Rigidbody2D not found");
         }
-        _viewCamera = Camera.main;
     }
 
     private void Update()
     {
         _velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * moveSpeed;
-        
-        // Rotate the player to look at the cursor
-        var mousePosition = _viewCamera.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 direction = (mousePosition - transform.position).normalized;
-        var angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, -angle));
+
+        UpdateSprite();
         
         if (Input.GetButtonDown("Sneak"))
         {
@@ -39,6 +40,28 @@ public class PlayerScript : MonoBehaviour
             enabled = false;
             
             StartCoroutine(Unhide());
+        }
+    }
+    
+    private void UpdateSprite()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            _spriteRenderer.sprite = sideSprite;
+            _spriteRenderer.flipX = false;
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            _spriteRenderer.sprite = sideSprite;
+            _spriteRenderer.flipX = true;
+        }
+        else if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            _spriteRenderer.sprite = upSprite;
+        }
+        else if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            _spriteRenderer.sprite = downSprite;
         }
     }
     
